61c0eade5772d5a88d996d0e78818883b9bb1cd3
xokn1032-create <xokn1032@icloud.com>
2026-09-20 09:02:53 -0600

    Update xokn1032_con.py

--- xokn1032_con.py
+++ xokn1032_con.py
@@ -19,12 +19,31 @@ class PyNcat:
         if self.args.verbose:
             logging.getLogger().setLevel(logging.DEBUG)
 
-def parse_args(self):
-    parser = argparse.ArgumentParser(description="PyNcat", 
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description="PyNcat", 
                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-c', '--connect', type=str, help='Connect to target (reverse shell)')
-    parser.add_argument('-v', '--verbose', action='store_true')
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('-c', '--connect', type=str, help='Connect to target (reverse shell)')
+        parser.add_argument('-v', '--verbose', action='store_true')
         
-    return parser.parse_args()
+        return parser.parse_args()
+
+    # === NETWORK RUNTIME ===
+    def start(self):
+        retries = 0
+        while True:
+            try:
+                if  self.args.connect:
+                    self.run_connector()
+                    if not self.args.persistent:
+                        break
+            except KeyboardInterrupt:
+                logging.info("\n[*] Exiting by user request.")
+                break
+            except Exception as e:
+                logging.error(f"Runtime error encountered: {e}")
+
+if __name__ == "__main__":
+    netcat = PyNcat()
+    netcat.start()
