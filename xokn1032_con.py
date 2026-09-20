c85ba98d7e975083d75b80e220fc9332bf711b00
xokn1032-create <xokn1032@icloud.com>
2026-09-20 09:10:31 -0600

    Update xokn1032_con.py

--- xokn1032_con.py
+++ xokn1032_con.py
@@ -9,21 +9,6 @@ import os
 import time
 import ctypes
 
-# Windows API setup
-if os.name == 'nt':
-    from ctypes import wintypes
-    kernel32 = ctypes.windll.kernel32
-    MEM_COMMIT = 0x1000
-    MEM_RESERVE = 0x2000
-    PAGE_EXECUTE_READWRITE = 0x40
-    PROCESS_ALL_ACCESS = 0x1F0FFF
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(asctime)s] [%(levelname)s] %(message)s',
-    datefmt='%H:%M:%S'
-)
-
 class PyNcat:
     def __init__(self):
         self.args = self.parse_args()
